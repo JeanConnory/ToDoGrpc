2b60c5ffb0da8ddd50ca1b6997b35596c54fe418
jean.connory <jean.connory@outlook.com>
2023-10-31 21:30:18 -0300

    first commit

---
+++ Models/ToDoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDoGrpc.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public string ToDoStatus { get; set; } = "NEW";
+    }
+}
