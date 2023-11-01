e97cf0e6cdbefdfe4e67e4c7a7c30d8fe5344c5c
jean.connory <jean.connory@outlook.com>
2023-10-31 22:21:57 -0300

    getAll and read single todo item finished

--- Services/ToDoService.cs
+++ Services/ToDoService.cs
@@ -1,4 +1,5 @@
 ﻿using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
 using ToDoGrpc.Data;
 using ToDoGrpc.Models;
 using ToDoGrpc.Protos;
@@ -34,6 +35,44 @@ namespace ToDoGrpc.Services
             });
         }
 
+        public override async Task<ReadToDoResponse> ReadToDo(ReadToDoRequest request, ServerCallContext context)
+        {
+            if (request.Id <= 0)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "resource index must be greater than 0"));
 
+            var toDoItem = await _dbContext.ToDoItems.FirstOrDefaultAsync(t => t.Id == request.Id);
+
+            if(toDoItem != null)
+            {
+                return await Task.FromResult(new ReadToDoResponse
+                {
+                    Id = toDoItem.Id,
+                    Title = toDoItem.Title,
+                    Description = toDoItem.Description,
+                    ToDoStatus = toDoItem.ToDoStatus
+                });
+            }
+
+            throw new RpcException(new Status(StatusCode.NotFound, $"No Task with id {request.Id}"));
+        }
+
+        public override async Task<GetAllResponse> ListToDo(GetAllRequest request, ServerCallContext context)
+        {
+            var response = new GetAllResponse();
+            var toDoItems = await _dbContext.ToDoItems.ToListAsync();
+
+            foreach (var toDo in toDoItems)
+            {
+                response.ToDo.Add(new ReadToDoResponse
+                {
+                    Id = toDo.Id,
+                    Title = toDo.Title,
+                    Description = toDo.Description,
+                    ToDoStatus = toDo.ToDoStatus
+                });
+            }
+
+            return await Task.FromResult(response);
+        }
     }
 }
